2c343b88c5c3f4a98b45a475fcfc9824f75d355b
Vang Huynh <vangpro1991@gmail.com>
2025-03-04 16:32:54 +0700

    logging and error handling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,6 +32,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/unbound-method': 'off',
     },
   },
 );
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(new LoggerMiddleware().use);
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(process.env.PORT ?? 4000);
 }
+
 bootstrap();
